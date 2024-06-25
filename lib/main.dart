9f5887f8de7b0065f71da8a27fad069cd1c4868f
dasun dhammasara <dasundhammasara9@gmail.com>
2024-06-25 08:08:27 +0530

    ui updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,25 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:file_picker/file_picker.dart';
-// import 'package:path_provider/path_provider.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(600, 800),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    title: 'Image Similarity Detection',
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(const MyApp());
 }
 
@@ -15,11 +31,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Image Similarity Detection',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(key: Key('myHomePage')),
+      home: const MyHomePage(),
     );
   }
 }
@@ -34,25 +51,31 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<String> _selectedImages = [];
   List<List<String>> _similarImages = [];
+  String? _directoryPath;
 
   Future<void> findSimilarImages(List<String> images) async {
+    _showLoadingDialog();
     const url = 'http://127.0.0.1:5000/find_similar_images';
-    final response = await http.post(
-      Uri.parse(url),
-      headers: {'Content-Type': 'application/json'},
-      body: json.encode({'images': images}),
-    );
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({'images': images}),
+      );
 
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      setState(() {
-        _similarImages = List<List<String>>.from(
-          data['similar_images'].map((group) => List<String>.from(group)),
-        );
-      });
-      debugPrint('Similar Images: $_similarImages');
-    } else {
-      debugPrint('Error: ${response.reasonPhrase}');
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          _similarImages = List<List<String>>.from(
+            data['similar_images'].map((group) => List<String>.from(group)),
+          );
+        });
+        debugPrint('Similar Images: $_similarImages');
+      } else {
+        debugPrint('Error: ${response.reasonPhrase}');
+      }
+    } finally {
+      Navigator.of(context).pop(); // Hide the loading dialog
     }
   }
 
@@ -69,24 +92,54 @@ class _MyHomePageState extends State<MyHomePage> {
       }
 
       setState(() {
+        _directoryPath = directoryPath;
         _selectedImages = imagePaths;
       });
-      findSimilarImages(_selectedImages);
     } else {
       debugPrint('No directory selected');
     }
+  }
+
+  void _showLoadingDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
   }
 
   Widget buildImageGrid(List<String> images) {
     return GridView.builder(
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 3,
-        crossAxisSpacing: 4.0,
-        mainAxisSpacing: 4.0,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
       ),
       itemCount: images.length,
       itemBuilder: (context, index) {
-        return Image.file(File(images[index]));
+        return Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                offset: Offset(2, 2),
+                blurRadius: 5,
+              ),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Image.file(
+              File(images[index]),
+              fit: BoxFit.cover,
+            ),
+          ),
+        );
       },
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
@@ -101,19 +154,79 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
                 onPressed: pickDirectory,
-                child: const Text('Select Directory'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  textStyle: const TextStyle(fontSize: 16),
+                ),
+                child: const Text(
+                  'Select Directory',
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
+              if (_directoryPath != null) ...[
+                const SizedBox(height: 20),
+                Text(
+                  'Selected Directory: $_directoryPath',
+                  style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () => findSimilarImages(_selectedImages),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent,
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    textStyle: const TextStyle(fontSize: 16),
+                  ),
+                  child: const Text(
+                    'Find Similar Images',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
               if (_similarImages.isNotEmpty) ...[
                 const SizedBox(height: 20),
-                const Text('Similar Image Groups:'),
-                for (var group in _similarImages) ...[
-                  buildImageGrid(group),
-                  const Divider(),
+                const Text(
+                  'Similar Image Groups:',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                for (var i = 0; i < _similarImages.length; i++) ...[
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 20),
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black,
+                          offset: Offset(2, 2),
+                          blurRadius: 5,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Group ${i + 1}',
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        buildImageGrid(_similarImages[i]),
+                      ],
+                    ),
+                  ),
                 ],
               ],
             ],
